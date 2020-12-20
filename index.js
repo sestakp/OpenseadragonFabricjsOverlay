621a90999970d24efd84816f486182762040aaed
Adam Arling <aarling@gmail.com>
2020-12-19 18:27:57 -0600

    Convert to exportable modules

---
+++ index.js
@@ -0,0 +1,7 @@
+const initFabricJSOverlay = require("./openseadragon-fabricjs-overlay");
+const fabric = require("./fabric/fabric.adapted").fabric;
+
+module.exports = {
+  initFabricJSOverlay,
+  fabric,
+};
